7c070ff4e66c2ce0758b8a6649b1b882117a0a10
ravindra-kale <ravindra.kale@thinkitive.com>
2022-02-02 13:58:28 +0530

    changes reflected

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 console.log("Hello World");
 console.log("there new statment");
-<<<<<<< HEAD
-=======
-console.log("new Change"):
->>>>>>> 00fe71d165c237dd00742a8d2c7ca60970605414
+console.log("ravi edited");
+console.log("Master Edited this file");
